73fedcd0d61541bb140ed8ffaab311166982aaf1
justomiguelvargas@gmail.com <justomiguelvargas@gmail.com@0f613677-0c5e-09d4-e9fd-41543678661e>
2012-11-05 02:43:45 +0000

    fixed

--- trunk/ga-solver/src/main/java/genetics/cromosomas/external/ExternalData.java
+++ trunk/ga-solver/src/main/java/genetics/cromosomas/external/ExternalData.java
@@ -5,6 +5,8 @@
 package genetics.cromosomas.external;
 
 import genetics.individuos.Individuo;
+import genetics.productos.Producto;
+import java.util.LinkedList;
 
 /**
  *
@@ -13,12 +15,32 @@ import genetics.individuos.Individuo;
 public class ExternalData {
 
     private int[] productS;
+    private LinkedList<LinkedList<Integer>> materiaPrima;
 
     public ExternalData(Individuo individuo) {
         int productsSize = individuo.getTotalDiferrentProducts();
         productS = new int[productsSize];
         for (int i = 0; i < productsSize; i++) {
             productS[i] = individuo.getProductsSize(i + 1);
+        }
+        //armo una lista donde voy a guardar las configuraciones por producto
+        materiaPrima = new LinkedList<LinkedList<Integer>>();
+        for (int i = 0; i < 4; i++) {
+            LinkedList<Integer> linkedList = new LinkedList<Integer>();
+            materiaPrima.add(linkedList);
+        }
+
+        for (int i = 0; i < productS.length; i++) {
+            int j = productS[i];
+            if (j > 0) {
+                Producto theProduct = individuo.getProductAt(i+1);
+                if (theProduct != null) {
+                    for (int k = 0; k < 8; k++) {
+                        int restriccionMinima = theProduct.getRestriccionesUsed()[k];
+                        materiaPrima.get(i).add(k, restriccionMinima);
+                    }
+                }
+            }
         }
     }
 
@@ -28,5 +50,13 @@ public class ExternalData {
 
     public void setProductS(int[] productS) {
         this.productS = productS;
+    }
+
+    public LinkedList<LinkedList<Integer>> getMateriaPrima() {
+        return materiaPrima;
+    }
+
+    public void setMateriaPrima(LinkedList<LinkedList<Integer>> materiaPrima) {
+        this.materiaPrima = materiaPrima;
     }
 }
